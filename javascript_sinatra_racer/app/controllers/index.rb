352d3b46ed36bd37878d1b96821be929b839a9bc
& <pair+LadyMozzarella.menor@devbootcamp.com>
2014-02-25 18:23:11 -0800

    Make first integration with Sinatra. It's a little messy, some active record validations don't work and needs a lot of polish, we also realized that we need to know about Ajax to make the backend and the frontend communicate.
     Please enter the commit message for your changes. Lines starting

--- javascript_sinatra_racer/app/controllers/index.rb
+++ javascript_sinatra_racer/app/controllers/index.rb
@@ -1,18 +1,34 @@
+get '/games/new' do
+  erb :game_board
+  # Play game here. Post game information to gameplayers
+  # through post '/games/new'.
+end
+
 get '/' do
-  # Look in app/views/index.erb
-  erb :index
+  erb :start_form
+  # Form for player 1 & 2 -> Post to '/players/new'
+  # to create the players.
 end
 
-get '/bands' do
-  @band_names = Band.all.map(&:name)
-  erb :bands
+post '/players/new' do
+  @player_one = Player.create(name: params[:player_one])
+  @player_two = Player.create(name: params[:player_two])
+  redirect '/games/new'
 end
 
-post '/bands' do
-  new_band = Band.create!(name: params[:name])
-  new_band.name
+
+post '/games/new' do
+  # Create gameplayers object and redirect to '/games/:id' to
+  # show the results.
 end
 
-get '/info' do
-  Demo.new(self).info
+get '/games/:id' do
+  # Show results of a particular game.
 end
+
+get '/games' do
+  # List all of the games played. Links to individual game
+  # scores.
+end
+
+
