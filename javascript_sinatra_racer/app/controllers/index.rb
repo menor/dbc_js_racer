b15b4e295be1d99b6410650b517f6191596bff89
& <pair+LadyMozzarella.menor@devbootcamp.com>
2014-02-25 20:23:53 -0800

    Databases working and correctly storing necessary information Player 2 is not working now

--- javascript_sinatra_racer/app/controllers/index.rb
+++ javascript_sinatra_racer/app/controllers/index.rb
@@ -1,25 +1,39 @@
-get '/games/new' do
-  erb :game_board
-  # Play game here. Post game information to gameplayers
-  # through post '/games/new'.
-end
-
 get '/' do
   erb :start_form
-  # Form for player 1 & 2 -> Post to '/players/new'
-  # to create the players.
 end
 
 post '/players/new' do
-  @player_one = Player.create(name: params[:player_one])
-  @player_two = Player.create(name: params[:player_two])
-  redirect '/games/new'
+  @player_one = Player.find_or_create_by_name(params[:player_one])
+  @player_two = Player.find_or_create_by_name(params[:player_two])
+  session[:p1_id] = @player_one.id
+  session[:p2_id] = @player_two.id
+  redirect "/games/new"
 end
 
+get '/games/new' do
+  erb :game_board
+end
 
-post '/games/new' do
-  # Create gameplayers object and redirect to '/games/:id' to
-  # show the results.
+post '/games/new' do #/player1_id=:player_1_id' do
+  @player_one = Player.find_by_id(session[:p1_id])
+  @player_two = Player.find_by_id(session[:p2_id])
+
+  @game = Game.create();
+  @game.players << @player_one
+  @game.players << @player_two
+
+  if params[:the_winner] == "player_one"
+    the_best = @game.gameplayers.find_by_player_id(@player_one.id)
+    the_best.winner = true
+    the_best.save
+    redirect '/'
+  else
+    the_best = @game.gameplayers.find_by_player_id(@player_two.id)
+    the_best.winner = true
+    the_best.save
+    redirect '/'
+  end
+  redirect '/'
 end
 
 get '/games/:id' do
