c54322d377af318e356a983738ae6755164d5163
& <pair+LadyMozzarella.menor@devbootcamp.com>
2014-02-25 20:32:03 -0800

    Fix Player 2 key press! Need to complete: Call system (exit & reload after completed game)                   Game list & individual game info pages

---
+++ javascript_sinatra_racer/public/js/view.js
@@ -0,0 +1,54 @@
+$(document).ready( function() {
+  createPlayers();
+
+  $(document).on('keyup', function(e) {
+    if (e.keyCode == 75) {
+      player2.position += 1;
+    }
+    if (e.keyCode == 83) {
+      player1.position += 1;
+    }
+
+    winning();
+    updatePlayerPositions();
+  });
+});
+
+function createPlayers() {
+  player1 = new Player;
+  player2 = new Player;
+}
+
+function updatePlayerPositions() {
+  $('.racer_table tr').children().removeClass('active');
+  $("#player1_strip td:nth-child(" + player1.position + ")").addClass('active');
+  $("#player2_strip td:nth-child(" + player2.position + ")").addClass('active');
+}
+
+function winning() {
+  if(player1.position == 21) {
+    // return player1 to Ajax
+    $.ajax({
+      type: "POST",
+      url: "/games/new",
+      data: { the_winner: "player_one"}
+    })
+    .done(function( msg ) {
+      alert( "Data Saved: " + msg );
+    });
+  }
+  if(player2.position == 21) {
+   // return player2 to Ajax
+    $.ajax({
+      type: "POST",
+      url: "/games/new",
+      data: { the_winner: "player_two" }
+    });
+  }
+}
+
+// .ajax {
+//   action "POST"
+//   URL "/"
+//   data {winner: player1.name } <----passes this hash as 'params'
+// }.done(msg){slkdglsdrghjl}
